e25884e88a00ad39a75f27f0ffbc9d6acc708047
Viennarz Curtiz <viennarzc@gmail.com>
2022-06-16 18:50:30 +0800

    Create project

---
+++ ShoppingTracker-SwiftCharts/ShoppingTracker-SwiftCharts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ShoppingTracker-SwiftCharts
+//
+//  Created by Viennarz Curtiz on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoppingTracker-SwiftCharts/ShoppingTracker-SwiftCharts/ShoppingTracker_SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingTracker_SwiftChartsApp.swift
+//  ShoppingTracker-SwiftCharts
+//
+//  Created by Viennarz Curtiz on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingTracker_SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
